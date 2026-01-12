5d331c61a068c150780193844acc56d2f7ac44c5
TanakaLun <2709410138@qq.com>
2026-01-13 00:13:36 +0800

    fix: import missing BorderStroke from material3

--- app/src/main/kotlin/io/tl/glowinghelper/ImageEditActivity.kt
+++ app/src/main/kotlin/io/tl/glowinghelper/ImageEditActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.BorderStroke
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
