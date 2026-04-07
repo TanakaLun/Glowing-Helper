3a118f1060abe3137ccd56143e4caaa000928190
TanakaLun <2709410138@qq.com>
2026-04-08 01:04:38 +0800

    chore: bump Java17 to Java25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
+    }
+    
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget("25"))
+        }
     }
     
 }
